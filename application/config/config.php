3c967539adda5201618fe01c93ae20a428d0bb00
Misael Flores <dydiar-flores@hotmail.com>
2018-11-03 11:59:32 -0600

    :-)

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Trabajo_FastCash/fast-cash/';
+$config['base_url'] = 'http://localhost/Trabajo_Fast_Cash/fast-cash/';
 // $config['base_url'] = 'http://localhost/Trabajo_Fast_Cash/fast-cash/';
 /*
 |--------------------------------------------------------------------------
